1cb369640fb2fa0357feaa74ef7243a0b36b28e3
yugandhara-salunkhe <yugandharasalunkhe49@gmail.com>
2025-03-25 12:43:10 +0530

    form commit

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
+const form = document.getElementById('Insurance-Form');
+
+form.addEventListener('submit',function(event){
+  event.preventDefault(); // to prevent from refreshing
+
+  const adult = document.getElementById('adult').value;
+  const kid = document.getElementById('kid').value;
+  const mobile = document.getElementById('mobile').value;
+  const email = document.getElementById('email').value;
+  const pincode = document.getElementById('pincode').value;
+  const city = document.getElementById('city').value;
+  const terms = document.getElementById('terms').checked;
+  const whatsapp = document.getElementById('whatsapp').checked;
+
+  let valid = true;
+
+  if(adult.trim() === ''){
+    alert('Name is required');
+    valid = false;
+  }
+  if(kid.trim() === ''){
+    alert('Name is required');
+    valid = false;
+  }
+  const mobileRegex = /^[0-9]{10}$/; // Simple check for a 10-digit number
+  if (!mobileRegex.test(mobile)) {
+    alert('Please enter a valid 10-digit mobile number');
+    valid = false;
+  }
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailRegex.test(email)) {
+    alert('Please enter a valid email address');
+    valid = false;
+  }
+
+
+  console.log('Adult Name:', adult);
+  console.log('Kid Name:', kid);
+  console.log('Mobile No.:', mobile);
+  console.log('Email Id:', email);
+  console.log('Pincode:', pincode);
+  console.log('City:', city);
+
+
+ form.reset();
+})
+
+
 const swiper = new Swiper('.mySwiper', {
   // Optional parameters
   direction: 'horizontal',
@@ -12,3 +60,5 @@ const swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev',
   }
 });
+
+
