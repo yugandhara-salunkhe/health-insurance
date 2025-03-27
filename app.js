d0522abe1a731511e77b7536580a46e2faa7f4b3
yugandhara-salunkhe <yugandharasalunkhe49@gmail.com>
2025-03-27 12:46:10 +0530

    register commit

--- app.js
+++ app.js
@@ -3,8 +3,65 @@
     navList.classList.toggle('active');
  }
 
+ function openModal(){
+  const modal = document.getElementById('loginModal');
+  modal.style.display = "block";
+ }
+
+ function closeModal(){
+  const modal = document.getElementById('loginModal');
+  modal.style.display = "none";
+ }
+
+ const registerform = document.getElementById('registrationForm');
+
+ registerform.addEventListener('submit',function(event){
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const mail = document.getElementById('Registeremail').value;
+  const mobile = document.getElementById('Registermobile').value;
+
+  let valid = true;
+
+  if(username.trim() === ''){
+    alert('Name is required');
+    valid = false;
+  }
+  const mobileRegex = /^[0-9]{10}$/; 
+  if (!mobileRegex.test(mobile)) {
+    alert('Please enter a valid 10-digit mobile number');
+    valid = false;
+  }
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailRegex.test(mail)) {
+    alert('Please enter a valid email address');
+    valid = false;
+  }
+
+  if(valid){
+    const regformData = {
+      username: username,
+      Registermobile: mobile,
+      Registeremail: mail,
+    };
+
+    localStorage.setItem('formData',JSON.stringify(regformData));
+    registerform.reset();
 
 
+    console.log(regformData);
+
+    alert("form data has been stored in local Storage");
+  }
+  else{
+    alert("please fix the Error");
+  }
+
+  console.log(regformData);
+
+
+ });
 
 const form = document.getElementById('Insurance-Form');
 
@@ -56,6 +113,8 @@ form.addEventListener('submit',function(event){
     };
 
     localStorage.setItem('formData',JSON.stringify(formData));
+    form.reset();
+
 
     console.log(formData);
 
@@ -65,17 +124,16 @@ form.addEventListener('submit',function(event){
     alert("please fix the Error:\n' " + errorMessages.join('\n'));
   }
 
+  console.log(formData);
+  // console.log('Adult Name:', adult);
+  // console.log('Kid Name:', kid);
+  // console.log('Mobile No.:', mobile);
+  // console.log('Email Id:', email);
+  // console.log('Pincode:', pincode);
+  // console.log('City:', city);
 
-  console.log('Adult Name:', adult);
-  console.log('Kid Name:', kid);
-  console.log('Mobile No.:', mobile);
-  console.log('Email Id:', email);
-  console.log('Pincode:', pincode);
-  console.log('City:', city);
 
-
- form.reset();
-});
+ });
 
 
 const swiper = new Swiper('.mySwiper', {
